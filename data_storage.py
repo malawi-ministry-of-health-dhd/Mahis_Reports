d21e70c773534aa466fd037f63c14fc0311ed855
Innocent Wowa <61548006+innowowa@users.noreply.github.com>
2026-06-13 19:17:18 +0200

    fixed data pull for single tables utilizing ssh and db config

--- data_storage.py
+++ data_storage.py
@@ -289,7 +289,9 @@ class DataStorage:
 
     def fetch_and_save_single_table(self, table_name, format="csv"):
         """Fetch fresh data from DB and save to CSV."""
-        fetcher = DataFetcher(use_localhost=USE_LOCALHOST)
+        fetcher = DataFetcher(use_localhost=USE_LOCALHOST, 
+                              sh_config=self.ssh_config, 
+                                db_config=self.db_config)
         return fetcher.fetch_single_table(
             query=self.query,
             table_name=table_name,
